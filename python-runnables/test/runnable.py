39bba152d6eb89522fc409aa0f01997486255d26
dku_u2 <dku_u2>
2020-07-07 05:45:13 +0000

    Edited files

--- python-runnables/test/runnable.py
+++ python-runnables/test/runnable.py
@@ -38,12 +38,12 @@ class MyRunnable(Runnable):
         try:
             client.get_connection(Src_Connection).get_info()
         except:
-            print(f"{Src_Connection} does not exist")
+            print({Src_Connection}," does not exist")
             
         try:
             client.get_connection(Dest_Connection).get_info()
         except:
-            print(f"{Dest_Connection} does not exist")    
+            print({Dest_Connection}," does not exist")    
         
         # check that destination connection is valid
      
