232908cb1ef0522b922fc51d5af28c047214e84d
dku_u2 <dku_u2>
2020-07-07 09:36:05 +0000

    Edited files

--- python-runnables/test/runnable.py
+++ python-runnables/test/runnable.py
@@ -46,7 +46,7 @@ class MyRunnable(Runnable):
             self.client.get_connection(Dest_Connection).get_info()
         except:
             print("Destination Connection does not exist")    
-            raise
+            raise Exception('spam', 'eggs')
        
      
         datasets = project.list_datasets()
